7b52cfd548875413df776add3e90bf4d2369248c
vrusi <me@vrusinkova.com>
2021-09-02 22:45:17 +0200

    Add room class

--- src/logic/game.ts
+++ src/logic/game.ts
@@ -3,6 +3,14 @@ type FieldType = 'Hall' | 'Door' | 'Room' | 'Inaccessible';
 type WeaponsMapType = Map<Room, Weapon | null>;
 type BoardWeapon = { weapon: Weapon, isSet: boolean };
 type BoardRoom = { room: Room, isTaken: boolean };
+type RoomName = 'Courtyard' | 'Game Room' | 'Study' | 'Dining Room' | 'Garage' | 'LivingRoom' | 'Kitchen' | 'Bedroom' | 'Bathroom';
+type Weapon = 'Rope' | 'Dagger' | 'Wrench' | 'Pistol' | 'Candlestick' | 'Lead Pipe';
+type CardType = 'Suspect' | 'Room' | 'Weapon';
+type CardValue = Suspect | Room | Weapon;
+type CharacterName = 'Plum' | 'White' | 'Scarlet' | 'Green' | 'Mustard' | 'Peacock';
+type Position = { row: number, column: number };
+type GameStatus = 'Created' | 'Playing' | 'Over';
+
 
 class Board {
     public fields: FieldType[][];
@@ -31,7 +39,9 @@ class Board {
     initWeapons() {
         this.weapons.forEach(
             weapon => {
-                let room = this.rooms[0];
+
+                let room;
+
                 do {
                     room = this.rooms[this.getRandomInt(0, this.rooms.length)];
 
@@ -45,11 +55,8 @@ class Board {
             }
         );
     }
-
-
 }
 
-type CharacterName = 'Plum' | 'White' | 'Scarlet' | 'Green' | 'Mustard' | 'Peacock';
 
 class Suspect {
     public name: CharacterName;
@@ -67,8 +74,6 @@ class Suspect {
     }
 }
 
-type Position = { row: number, column: number };
-
 class Player {
     public character: Suspect;
     public cards: Card[];
@@ -83,12 +88,35 @@ class Player {
         this.cards = cards;
         this.position = position;
     }
+
+    // move
+
+    // ask
+
+    // accuse
+
+    // mark
 }
 
-type Room = 'Courtyard' | 'Game Room' | 'Study' | 'Dining Room' | 'Garage' | 'LivingRoom' | 'Kitchen' | 'Bedroom' | 'Bathroom';
-type Weapon = 'Rope' | 'Dagger' | 'Wrench' | 'Pistol' | 'Candlestick' | 'Lead Pipe';
-type CardType = 'Suspect' | 'Room' | 'Weapon';
-type CardValue = Suspect | Room | Weapon;
+class Room {
+    public name: RoomName;
+    public weapons: Weapon[];
+    public suspects: Suspect[];
+    public entrances: Position[];
+
+    constructor(
+        name: RoomName,
+        weapons: Weapon[] = [],
+        suspects: Suspect[] = [],
+        entrances: Position[],
+    ) {
+        this.name = name;
+        this.weapons = weapons;
+        this.suspects = suspects;
+        this.entrances = entrances;
+    }
+}
+
 
 class Card {
     public type: CardType;
@@ -106,20 +134,26 @@ class Card {
     }
 }
 
+
 export default class Game {
     public board: Board;
     public players: Player[];
     public cards: Card[];
+    public status: GameStatus;
 
     constructor(
         board: Board,
         players: Player[],
         cards: Card[],
+        status: GameStatus = 'Created',
     ) {
         this.board = board;
         this.players = players;
         this.cards = cards;
+        this.status = status;
     }
 
-
+    public init() {
+        this.board.initWeapons();
+    }
 }
